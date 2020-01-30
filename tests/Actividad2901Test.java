165fd7399e11f358fbd3fc1bb27911126d88aae5
Miguel Bayón <miguel.bayon@iessanandres.com>
2020-01-30 10:15:27 +0100

    Create Actividad2901Test.java

---
+++ tests/Actividad2901Test.java
@@ -0,0 +1,23 @@
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Actividad2901Test {
+
+    @Test
+    public void testValoresNormales() {
+        Actividad2901 objetoBase = new Actividad2901();
+        assertEquals(90, objetoBase.sumaDeMultiplos(2, 20));
+        assertEquals(18, objetoBase.sumaDeMultiplos(3, 10));
+        assertEquals(4950, objetoBase.sumaDeMultiplos(1, 100));
+        assertEquals(75, objetoBase.sumaDeMultiplos(5, 30));
+    }
+    
+    @Test
+    public void testValoresAnormales() {
+        Actividad2901 objetoBase = new Actividad2901();
+        assertEquals(-1, objetoBase.sumaDeMultiplos(-5, 32));
+        assertEquals(-1, objetoBase.sumaDeMultiplos(0, 100));
+    }    
+}
