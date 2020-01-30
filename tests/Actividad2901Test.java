f842cb865eecf5aaa0a7b9bd6765e6b4b28bcf32
Miguel Bayón <miguel.bayon@iessanandres.com>
2020-01-30 10:18:55 +0100

    Update Actividad2901Test.java

--- tests/Actividad2901Test.java
+++ tests/Actividad2901Test.java
@@ -9,7 +9,7 @@ public class Actividad2901Test {
     public void testValoresNormales() {
         Actividad2901 objetoBase = new Actividad2901();
         assertEquals(90, objetoBase.sumaDeMultiplos(2, 20));
-        assertEquals(18, objetoBase.sumaDeMultiplos(3, 10));
+        assertEquals(18, objetoBase.sumaDeMultiplos(3, 9));
         assertEquals(4950, objetoBase.sumaDeMultiplos(1, 100));
         assertEquals(75, objetoBase.sumaDeMultiplos(5, 30));
     }
@@ -19,5 +19,7 @@ public class Actividad2901Test {
         Actividad2901 objetoBase = new Actividad2901();
         assertEquals(-1, objetoBase.sumaDeMultiplos(-5, 32));
         assertEquals(-1, objetoBase.sumaDeMultiplos(0, 100));
+        assertEquals(-1, objetoBase.sumaDeMultiplos(3, 100));
+        assertEquals(-1, objetoBase.sumaDeMultiplos(8, 100));
     }    
 }
