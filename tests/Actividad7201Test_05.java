526094f5beb850e2a4b27d52c1bff4dc912d38f8
Miguel Bayón <miguel.bayon@iessanandres.com>
2021-03-19 13:52:20 +0100

    Update Actividad7201Test_05.java

--- tests/Actividad7201Test_05.java
+++ tests/Actividad7201Test_05.java
@@ -1,6 +1,5 @@
-import org.junit.Test;
-
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Actividad7201Test_05
 {
