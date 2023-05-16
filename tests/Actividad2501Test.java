0a01f860c9336d7f2e1ae006935fd13d6e8311bc
Miguel Bayón <miguel.bayon@iessanandres.com>
2023-05-16 09:22:12 +0200

    Update Actividad2501Test.java

--- tests/Actividad2501Test.java
+++ tests/Actividad2501Test.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class Actividad2501Tests {
+public class Actividad2501Test {
 
     private Actividad2501 objetoBase;
 
