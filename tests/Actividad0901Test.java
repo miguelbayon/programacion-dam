aeb567926d2837c31fac92cd45bd21ce8108e1b6
Miguel Bayón <miguel.bayon@iessanandres.com>
2024-04-19 12:18:18 +0200

    Update Actividad0901Test.java

--- tests/Actividad0901Test.java
+++ tests/Actividad0901Test.java
@@ -2,45 +2,12 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class Actividad0901Test {
-
-    private String obtenido;
-
-    @Test
-    void testEstadoInicial() {
-        Granja granja = new Granja();
-        Pollo polloNormal = new Pollo(1,false);
-        Pollo polloModificado = new Pollo(2,true);
-        Pato patoNormal = new Pato(3, false);
-        Pato patoModificado = new Pato(4, true);
-        Cerdo cerdoNormal = new Cerdo(5, 4);
-        Cerdo cerdoDeRaza = new Cerdo(6, 8);
-        Vaca vacaNormal = new Vaca(7, 3);
-        Vaca vacaDeRaza = new Vaca(8, 9);
-
-        System.out.println("Estado inicial de la granja:");
-        System.out.println(polloNormal);
-        System.out.println(polloModificado);
-        System.out.println(patoNormal);
-        System.out.println(patoModificado);
-        System.out.println(cerdoNormal);
-        System.out.println(cerdoDeRaza);
-        System.out.println(vacaNormal);
-        System.out.println(vacaDeRaza);
-
-    }
-
-    @Test
-    void testEstadoInicial2() {import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class GranjaTest {
 
     private String obtenido;
 
     @Test
-    void testEstadoInicial() {
+    void testEstadoInicialPorPantalla() {
         Granja granja = new Granja();
         Pollo polloNormal = new Pollo(1,false);
         Pollo polloModificado = new Pollo(2,true);
@@ -64,7 +31,7 @@ public class GranjaTest {
     }
 
     @Test
-    void testEstadoInicial2() {
+    void testEstadoInicialComprobaciones() {
         Granja granja = new Granja();
         Pollo polloNormal = new Pollo(1,false);
         Pollo polloModificado = new Pollo(2,true);
@@ -92,7 +59,7 @@ public class GranjaTest {
 
 
     @Test
-    void testAlimentar() {
+    void testAlimentarPorPantalla() {
         Granja granja = new Granja();
         Pollo polloNormal = new Pollo(1,false);
         Pollo polloModificado = new Pollo(2,true);
@@ -141,7 +108,7 @@ public class GranjaTest {
     }
 
     @Test
-    void testAlimentar2() {
+    void testAlimentarComprobaciones() {
         Granja granja = new Granja();
         Pollo polloNormal = new Pollo(1,false);
         Pollo polloModificado = new Pollo(2,true);
@@ -185,45 +152,15 @@ public class GranjaTest {
                 "[VACA ]: ID: 8 - Peso: 27 kg. - Puntos de vida: 99 - Calidad de raza: 9\n";
 
         obtenido = "" + polloNormal + "\n" + polloModificado + "\n" + patoNormal + "\n" + patoModificado + "\n" + cerdoNormal + "\n" + cerdoDeRaza + "\n" + vacaNormal + "\n" + vacaDeRaza + "\n";
-
         assertEquals(esperado, obtenido);
 
     }
-}
-
-        Granja granja = new Granja();
-        Pollo polloNormal = new Pollo(1,false);
-        Pollo polloModificado = new Pollo(2,true);
-        Pato patoNormal = new Pato(3, false);
-        Pato patoModificado = new Pato(4, true);
-        Cerdo cerdoNormal = new Cerdo(5, 4);
-        Cerdo cerdoDeRaza = new Cerdo(6, 8);
-        Vaca vacaNormal = new Vaca(7, 3);
-        Vaca vacaDeRaza = new Vaca(8, 9);
-
-        String esperado = "" +
-                "[POLLO]: ID: 1 - Peso: 1 kg. - Puntos de vida: 100 - Modificado geneticamente: No\n" +
-                "[POLLO]: ID: 2 - Peso: 1 kg. - Puntos de vida: 100 - Modificado geneticamente: Si\n" +
-                "[PATO ]: ID: 3 - Peso: 2 kg. - Puntos de vida: 100 - Modificado geneticamente: No\n" +
-                "[PATO ]: ID: 4 - Peso: 2 kg. - Puntos de vida: 100 - Modificado geneticamente: Si\n" +
-                "[CERDO]: ID: 5 - Peso: 15 kg. - Puntos de vida: 100 - Calidad de raza: 4\n" +
-                "[CERDO]: ID: 6 - Peso: 15 kg. - Puntos de vida: 100 - Calidad de raza: 8\n" +
-                "[VACA ]: ID: 7 - Peso: 25 kg. - Puntos de vida: 100 - Calidad de raza: 3\n" +
-                "[VACA ]: ID: 8 - Peso: 25 kg. - Puntos de vida: 100 - Calidad de raza: 9\n";
-        String obtenido = "" + polloNormal + "\n" + polloModificado + "\n" + patoNormal + "\n" + patoModificado + "\n" + cerdoNormal + "\n" + cerdoDeRaza + "\n" + vacaNormal + "\n" + vacaDeRaza + "\n";
-        assertEquals(esperado, obtenido);
-
-    }
-
-
 
     @Test
-    void testAlimentar() {
+    void testVacunarPorPantalla() {
         Granja granja = new Granja();
         Pollo polloNormal = new Pollo(1,false);
         Pollo polloModificado = new Pollo(2,true);
-        Pato patoNormal = new Pato(3, false);
-        Pato patoModificado = new Pato(4, true);
         Cerdo cerdoNormal = new Cerdo(5, 4);
         Cerdo cerdoDeRaza = new Cerdo(6, 8);
         Vaca vacaNormal = new Vaca(7, 3);
@@ -231,48 +168,93 @@ public class GranjaTest {
 
         System.out.println("Estado inicial de la granja:");
 
+
         System.out.println(polloNormal);
         System.out.println(polloModificado);
-        System.out.println(patoNormal);
-        System.out.println(patoModificado);
         System.out.println(cerdoNormal);
         System.out.println(cerdoDeRaza);
         System.out.println(vacaNormal);
         System.out.println(vacaDeRaza);
 
         System.out.println();
+        System.out.println("___Pollo normal___");
+
+        System.out.println();
         granja.alimentar(polloNormal);
+        System.out.println();
+        granja.alimentar(polloNormal);
+        System.out.println();
+        granja.alimentar(polloNormal);
+        System.out.println();
+        granja.vacunar(polloNormal);
+
+        System.out.println();
+        System.out.println("___Pollo modificado___");
 
         System.out.println();
         granja.alimentar(polloModificado);
+        System.out.println();
+        granja.alimentar(polloModificado);
+        System.out.println();
+        granja.alimentar(polloModificado);
+        System.out.println();
+        granja.vacunar(polloModificado);
 
         System.out.println();
-        granja.alimentar(patoNormal);
-
-        System.out.println();
-        granja.alimentar(patoModificado);
+        System.out.println("___Cerdo normal___");
 
         System.out.println();
         granja.alimentar(cerdoNormal);
+        System.out.println();
+        granja.alimentar(cerdoNormal);
+        System.out.println();
+        granja.alimentar(cerdoNormal);
+        System.out.println();
+        granja.vacunar(cerdoNormal);
+
+        System.out.println();
+        System.out.println("___Cerdo de raza___");
 
         System.out.println();
         granja.alimentar(cerdoDeRaza);
+        System.out.println();
+        granja.alimentar(cerdoDeRaza);
+        System.out.println();
+        granja.alimentar(cerdoDeRaza);
+        System.out.println();
+        granja.vacunar(cerdoDeRaza);
+
+        System.out.println();
+        System.out.println("___Vaca normal___");
 
         System.out.println();
         granja.alimentar(vacaNormal);
+        System.out.println();
+        granja.alimentar(vacaNormal);
+        System.out.println();
+        granja.alimentar(vacaNormal);
+        System.out.println();
+        granja.vacunar(vacaNormal);
+
+        System.out.println();
+        System.out.println("___Vaca de raza___");
 
         System.out.println();
         granja.alimentar(vacaDeRaza);
+        System.out.println();
+        granja.alimentar(vacaDeRaza);
+        System.out.println();
+        granja.alimentar(vacaDeRaza);
+        System.out.println();
+        granja.vacunar(vacaDeRaza);
 
     }
 
     @Test
-    void testAlimentar2() {
+    void testVacunarComprobaciones() {
         Granja granja = new Granja();
         Pollo polloNormal = new Pollo(1,false);
         Pollo polloModificado = new Pollo(2,true);
-        Pato patoNormal = new Pato(3, false);
-        Pato patoModificado = new Pato(4, true);
         Cerdo cerdoNormal = new Cerdo(5, 4);
         Cerdo cerdoDeRaza = new Cerdo(6, 8);
         Vaca vacaNormal = new Vaca(7, 3);
@@ -281,38 +263,114 @@ public class GranjaTest {
         String esperado = "" +
                 "[POLLO]: ID: 1 - Peso: 1 kg. - Puntos de vida: 100 - Modificado geneticamente: No\n" +
                 "[POLLO]: ID: 2 - Peso: 1 kg. - Puntos de vida: 100 - Modificado geneticamente: Si\n" +
-                "[PATO ]: ID: 3 - Peso: 2 kg. - Puntos de vida: 100 - Modificado geneticamente: No\n" +
-                "[PATO ]: ID: 4 - Peso: 2 kg. - Puntos de vida: 100 - Modificado geneticamente: Si\n" +
                 "[CERDO]: ID: 5 - Peso: 15 kg. - Puntos de vida: 100 - Calidad de raza: 4\n" +
                 "[CERDO]: ID: 6 - Peso: 15 kg. - Puntos de vida: 100 - Calidad de raza: 8\n" +
                 "[VACA ]: ID: 7 - Peso: 25 kg. - Puntos de vida: 100 - Calidad de raza: 3\n" +
                 "[VACA ]: ID: 8 - Peso: 25 kg. - Puntos de vida: 100 - Calidad de raza: 9\n";
-        String obtenido = "" + polloNormal + "\n" + polloModificado + "\n" + patoNormal + "\n" + patoModificado + "\n" + cerdoNormal + "\n" + cerdoDeRaza + "\n" + vacaNormal + "\n" + vacaDeRaza + "\n";
+        String obtenido = "" + polloNormal + "\n" + polloModificado + "\n" + cerdoNormal + "\n" + cerdoDeRaza + "\n" + vacaNormal + "\n" + vacaDeRaza + "\n";
         assertEquals(esperado, obtenido);
 
-
+        //Pollo normal
         granja.alimentar(polloNormal);
-        granja.alimentar(polloModificado);
-        granja.alimentar(patoNormal);
-        granja.alimentar(patoModificado);
-        granja.alimentar(cerdoNormal);
-        granja.alimentar(cerdoDeRaza);
-        granja.alimentar(vacaNormal);
-        granja.alimentar(vacaDeRaza);
+        granja.alimentar(polloNormal);
+        granja.alimentar(polloNormal);
+        granja.vacunar(polloNormal);
 
         esperado = "" +
-                "[POLLO]: ID: 1 - Peso: 2 kg. - Puntos de vida: 90 - Modificado geneticamente: No\n" +
-                "[POLLO]: ID: 2 - Peso: 2 kg. - Puntos de vida: 98 - Modificado geneticamente: Si\n" +
-                "[PATO ]: ID: 3 - Peso: 3 kg. - Puntos de vida: 90 - Modificado geneticamente: No\n" +
-                "[PATO ]: ID: 4 - Peso: 3 kg. - Puntos de vida: 97 - Modificado geneticamente: Si\n" +
-                "[CERDO]: ID: 5 - Peso: 17 kg. - Puntos de vida: 90 - Calidad de raza: 4\n" +
-                "[CERDO]: ID: 6 - Peso: 17 kg. - Puntos de vida: 98 - Calidad de raza: 8\n" +
-                "[VACA ]: ID: 7 - Peso: 27 kg. - Puntos de vida: 90 - Calidad de raza: 3\n" +
-                "[VACA ]: ID: 8 - Peso: 27 kg. - Puntos de vida: 99 - Calidad de raza: 9\n";
+                "[POLLO]: ID: 1 - Peso: 4 kg. - Puntos de vida: 80 - Modificado geneticamente: No\n" +
+                "[POLLO]: ID: 2 - Peso: 1 kg. - Puntos de vida: 100 - Modificado geneticamente: Si\n" +
+                "[CERDO]: ID: 5 - Peso: 15 kg. - Puntos de vida: 100 - Calidad de raza: 4\n" +
+                "[CERDO]: ID: 6 - Peso: 15 kg. - Puntos de vida: 100 - Calidad de raza: 8\n" +
+                "[VACA ]: ID: 7 - Peso: 25 kg. - Puntos de vida: 100 - Calidad de raza: 3\n" +
+                "[VACA ]: ID: 8 - Peso: 25 kg. - Puntos de vida: 100 - Calidad de raza: 9\n";
 
-        obtenido = "" + polloNormal + "\n" + polloModificado + "\n" + patoNormal + "\n" + patoModificado + "\n" + cerdoNormal + "\n" + cerdoDeRaza + "\n" + vacaNormal + "\n" + vacaDeRaza + "\n";
-
+        obtenido = "" + polloNormal + "\n" + polloModificado + "\n" + cerdoNormal + "\n" + cerdoDeRaza + "\n" + vacaNormal + "\n" + vacaDeRaza + "\n";
         assertEquals(esperado, obtenido);
+
+        //Pollo modificado
+        granja.alimentar(polloModificado);
+        granja.alimentar(polloModificado);
+        granja.alimentar(polloModificado);
+        granja.vacunar(polloModificado);
+
+        esperado = "" +
+                "[POLLO]: ID: 1 - Peso: 4 kg. - Puntos de vida: 80 - Modificado geneticamente: No\n" +
+                "[POLLO]: ID: 2 - Peso: 4 kg. - Puntos de vida: 100 - Modificado geneticamente: Si\n" +
+                "[CERDO]: ID: 5 - Peso: 15 kg. - Puntos de vida: 100 - Calidad de raza: 4\n" +
+                "[CERDO]: ID: 6 - Peso: 15 kg. - Puntos de vida: 100 - Calidad de raza: 8\n" +
+                "[VACA ]: ID: 7 - Peso: 25 kg. - Puntos de vida: 100 - Calidad de raza: 3\n" +
+                "[VACA ]: ID: 8 - Peso: 25 kg. - Puntos de vida: 100 - Calidad de raza: 9\n";
+
+        obtenido = "" + polloNormal + "\n" + polloModificado + "\n" + cerdoNormal + "\n" + cerdoDeRaza + "\n" + vacaNormal + "\n" + vacaDeRaza + "\n";
+        assertEquals(esperado, obtenido);
+
+        //Cerdo normal
+        granja.alimentar(cerdoNormal);
+        granja.alimentar(cerdoNormal);
+        granja.alimentar(cerdoNormal);
+        granja.vacunar(cerdoNormal);
+
+        esperado = "" +
+                "[POLLO]: ID: 1 - Peso: 4 kg. - Puntos de vida: 80 - Modificado geneticamente: No\n" +
+                "[POLLO]: ID: 2 - Peso: 4 kg. - Puntos de vida: 100 - Modificado geneticamente: Si\n" +
+                "[CERDO]: ID: 5 - Peso: 21 kg. - Puntos de vida: 90 - Calidad de raza: 4\n" +
+                "[CERDO]: ID: 6 - Peso: 15 kg. - Puntos de vida: 100 - Calidad de raza: 8\n" +
+                "[VACA ]: ID: 7 - Peso: 25 kg. - Puntos de vida: 100 - Calidad de raza: 3\n" +
+                "[VACA ]: ID: 8 - Peso: 25 kg. - Puntos de vida: 100 - Calidad de raza: 9\n";
+
+        obtenido = "" + polloNormal + "\n" + polloModificado + "\n" + cerdoNormal + "\n" + cerdoDeRaza + "\n" + vacaNormal + "\n" + vacaDeRaza + "\n";
+        assertEquals(esperado, obtenido);
+
+        //Cerdo de raza
+        granja.alimentar(cerdoDeRaza);
+        granja.alimentar(cerdoDeRaza);
+        granja.alimentar(cerdoDeRaza);
+        granja.vacunar(cerdoDeRaza);
+
+        esperado = "" +
+                "[POLLO]: ID: 1 - Peso: 4 kg. - Puntos de vida: 80 - Modificado geneticamente: No\n" +
+                "[POLLO]: ID: 2 - Peso: 4 kg. - Puntos de vida: 100 - Modificado geneticamente: Si\n" +
+                "[CERDO]: ID: 5 - Peso: 21 kg. - Puntos de vida: 90 - Calidad de raza: 4\n" +
+                "[CERDO]: ID: 6 - Peso: 21 kg. - Puntos de vida: 100 - Calidad de raza: 8\n" +
+                "[VACA ]: ID: 7 - Peso: 25 kg. - Puntos de vida: 100 - Calidad de raza: 3\n" +
+                "[VACA ]: ID: 8 - Peso: 25 kg. - Puntos de vida: 100 - Calidad de raza: 9\n";
+
+        obtenido = "" + polloNormal + "\n" + polloModificado + "\n" + cerdoNormal + "\n" + cerdoDeRaza + "\n" + vacaNormal + "\n" + vacaDeRaza + "\n";
+        assertEquals(esperado, obtenido);
+
+        //Vaca normal
+        granja.alimentar(vacaNormal);
+        granja.alimentar(vacaNormal);
+        granja.alimentar(vacaNormal);
+        granja.vacunar(vacaNormal);
+
+        esperado = "" +
+                "[POLLO]: ID: 1 - Peso: 4 kg. - Puntos de vida: 80 - Modificado geneticamente: No\n" +
+                "[POLLO]: ID: 2 - Peso: 4 kg. - Puntos de vida: 100 - Modificado geneticamente: Si\n" +
+                "[CERDO]: ID: 5 - Peso: 21 kg. - Puntos de vida: 90 - Calidad de raza: 4\n" +
+                "[CERDO]: ID: 6 - Peso: 21 kg. - Puntos de vida: 100 - Calidad de raza: 8\n" +
+                "[VACA ]: ID: 7 - Peso: 31 kg. - Puntos de vida: 100 - Calidad de raza: 3\n" +
+                "[VACA ]: ID: 8 - Peso: 25 kg. - Puntos de vida: 100 - Calidad de raza: 9\n";
+
+        obtenido = "" + polloNormal + "\n" + polloModificado + "\n" + cerdoNormal + "\n" + cerdoDeRaza + "\n" + vacaNormal + "\n" + vacaDeRaza + "\n";        assertEquals(esperado, obtenido);
+
+        //Vaca de raza
+        granja.alimentar(vacaDeRaza);
+        granja.alimentar(vacaDeRaza);
+        granja.alimentar(vacaDeRaza);
+        granja.vacunar(vacaDeRaza);
+
+        esperado = "" +
+                "[POLLO]: ID: 1 - Peso: 4 kg. - Puntos de vida: 80 - Modificado geneticamente: No\n" +
+                "[POLLO]: ID: 2 - Peso: 4 kg. - Puntos de vida: 100 - Modificado geneticamente: Si\n" +
+                "[CERDO]: ID: 5 - Peso: 21 kg. - Puntos de vida: 90 - Calidad de raza: 4\n" +
+                "[CERDO]: ID: 6 - Peso: 21 kg. - Puntos de vida: 100 - Calidad de raza: 8\n" +
+                "[VACA ]: ID: 7 - Peso: 31 kg. - Puntos de vida: 100 - Calidad de raza: 3\n" +
+                "[VACA ]: ID: 8 - Peso: 31 kg. - Puntos de vida: 100 - Calidad de raza: 9\n";
+
+        obtenido = "" + polloNormal + "\n" + polloModificado + "\n" + cerdoNormal + "\n" + cerdoDeRaza + "\n" + vacaNormal + "\n" + vacaDeRaza + "\n";
+        assertEquals(esperado, obtenido);
+
 
     }
 }
